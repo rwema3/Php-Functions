cf9c437c615e4f3bdab382ca7ecaa84d053f2e89
rwema3 <rwemadominik3@gmail.com>
2022-01-24 09:53:35 +0200

    NCS system
    NCS system

--- bill.php
+++ bill.php
@@ -191,7 +191,7 @@ $tot_amount_not=0;
                                             // echo'<td class="cent">'.$defrowpe['qtty'].'</td>';                                            
                                             // echo'<td class="cent">'.$defrowpe['date'].'</td>';                                            
                                             // echo'</form>';
-                                            // echo 'tble row';	
+                                            // echo '</tr>';	
                                             }
 				   $tot_amount+=$tot;
 				   if ($insured==0)
