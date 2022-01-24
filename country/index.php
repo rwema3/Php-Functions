829c78367af78a182b076b11f105aa9255463829
rwema3 <rwemadominik3@gmail.com>
2022-01-24 10:03:00 +0200

    NCS system
    NCS system

--- country/index.php
+++ country/index.php
@@ -4,7 +4,7 @@
 <head>
 <style>
 body{width:611px;}
-.frmSearch {border: 1px solid #a8d4b1;background-color: #c6f7d0;margin: 2px 0px;padding:40px;border-radius:4px;}
+.frmSearch {border: 0px solid #a8d4b1;background-color: #c6f7d0;margin: 2px 0px;padding:40px;border-radius:4px;}
 #country-list{float:left;list-style:none;margin-top:-3px;padding:0;width:190px;position: absolute;}
 #country-list li{padding: 10px; background: #f0f0f0; border-bottom: #bbb9b9 1px solid;}
 #country-list li:hover{background:#ece3d2;cursor: pointer;}
