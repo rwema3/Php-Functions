8d85973c00dea5bc6d79ac2bf746c95422648c51
rwema3 <rwemadominik3@gmail.com>
2022-02-07 18:38:56 +0200

    Update index (1).php

--- index (1).php
+++ index (1).php
@@ -7,7 +7,7 @@ $msg=$_GET['msg'];
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 <link href="css.css" rel="stylesheet" type="text/css" media="screen" />
    <link rel="stylesheet" href="styles.css">
-   <script src="http://code.jquery.c
+   <script src="http://code.jquery
    <script src="script.js"></script>        
 <title>Med E-Pay</title>
 <style>
