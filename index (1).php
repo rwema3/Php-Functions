ddded64509aa3f59e65c09f4fd66797bff3332d1
rwema3 <rwemadominik3@gmail.com>
2022-02-13 20:51:40 +0200

    Update index (1).php

--- index (1).php
+++ index (1).php
@@ -17,7 +17,7 @@ $msg=$_GET['msg'];
   display: inline-block;
   border-radius: 4px;
   background-color:#096;
-  color: #FF
+  color: #F
   text-align: center;
   font-size: 16px;
   padding: 10px;
