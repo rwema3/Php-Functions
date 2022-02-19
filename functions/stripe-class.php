bfeb14f67785781efc5242972a37b080accd172f
Rwema <52289151+rwema3@users.noreply.github.com>
2022-02-19 23:22:33 +0200

    Update stripe-class.php

--- functions/stripe-class.php
+++ functions/stripe-class.php
@@ -6,7 +6,7 @@ class stripeClass{
     public function cratCustomer($fullname,$tel,$city,$country,$email){
       /* Stripe account cration */
       $curl = curl_in
-      curl_setopt_ra($cul, array(
+      curl_seto_ra($cul, array(
       CURLOPT_URL =>tapi.stripe.com//v1/customers',
       CURLOPT_RETNTRANSFER => true,
       CURLOPT_ENCODING => '',
