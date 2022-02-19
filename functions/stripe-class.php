6994f83e106ad78ece0b7d7f9f3ce1bcf5223326
Rwema <52289151+rwema3@users.noreply.github.com>
2022-02-19 23:19:20 +0200

    hhf

--- functions/stripe-class.php
+++ functions/stripe-class.php
@@ -15,7 +15,7 @@ class stripeClass{
       CURLOPT_FOLLOWLOCATION => true,
       CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
       CURLOPT_CUSOMREQUEST => 'POST',
-      CURLOPT_POSTFIELDS => '&email='.$email.'&address%5Bcity%5D='.$city.'&address%5Bcountry%5D='.$country.'&address%5Bline1%5D=KG%20666%20St&balance=1500&name='.$fullname.'&phone='.$tel,
+      CURLOPT_POSTFIELD=> '&email='.$email.'&address%5Bcity%5D='.$city.'&address%5Bcountry%5D='.$country.'&address%5Bline1%5D=KG%20666%20St&balance=1500&name='.$fullname.'&phone='.$tel,
       CURLOPT_HTTPHEADER => array(
          'Content-Type: applcation/x-www-form-urlencoded',
          'Authorization: Bearr sk_test_51KBzxXHR1XVDkcETd9dpbkV8xEyvRQmFPShqghPWRuNi2Y0zXFaWGL0GZFNEY5Cm9ua8ibow7kcjGRnpjpIprkRV00lLiD3eGN'
